04c77515bdfd591b038d05661132020d9ad4ad1d
Jakub G <gorzkiewicz84@gmail.com>
2023-11-04 08:20:27 +0100

    Initial commit

---
+++ app/src/main/java/com/jg/QrBusinessCard/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.jg.QrBusinessCard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.jg.QrBusinessCard.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setNavigation()
+
+
+    }
+
+
+    private fun setNavigation(){
+
+        val navHostFragment=supportFragmentManager.findFragmentById(R.id.baseFragment) as NavHostFragment
+        val fragments = mapOf(
+            R.id.businessCardListFragment to "businessCardListFragment",
+            R.id.qrCodeFragment to "qrCodeFragment",
+        )
+
+        binding.bottomAppBar.setOnItemSelectedListener{
+                item-> fragments[item.itemId]?.let{
+            navHostFragment.navController.navigate(item.itemId)
+            true
+        }?:false
+        }
+
+    }
+
+
+
+}
