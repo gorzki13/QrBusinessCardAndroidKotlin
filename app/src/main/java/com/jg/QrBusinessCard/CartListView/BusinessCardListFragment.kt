14744cdb25b158e3fa3a1f5edc6e05b5cf43facf
Jakub G <gorzkiewicz84@gmail.com>
2023-11-11 13:30:50 +0100

    Feat: BarcodeScanner

--- app/src/main/java/com/jg/QrBusinessCard/CartListView/BusinessCardListFragment.kt
+++ app/src/main/java/com/jg/QrBusinessCard/CartListView/BusinessCardListFragment.kt
@@ -2,17 +2,27 @@ package com.jg.QrBusinessCard.CartListView
 
 import android.annotation.SuppressLint
 import android.app.Dialog
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.SurfaceHolder
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
 import com.jg.QrBusinessCard.R
 import com.jg.QrBusinessCard.databinding.FragmentBusinessCardListBinding
+import java.io.IOException
 
 class BusinessCardListFragment : Fragment() {
     // TODO: Rename and change types of parameters
@@ -21,13 +31,20 @@ class BusinessCardListFragment : Fragment() {
     private lateinit var adapter: BusinessCardListAdapter
     private lateinit var binding: FragmentBusinessCardListBinding
     private val viewModel: BusinessCardListViewModel by activityViewModels()
-
+    private lateinit var cameraSource: CameraSource
+    private lateinit var barcodeDetector: BarcodeDetector
+    private var scannedValue = ""
+    private val requestCodeCameraPermission = 1001
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
 
         }
+
+
     }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,7 +59,14 @@ class BusinessCardListFragment : Fragment() {
         binding.businessCardRV.adapter = adapter
         // Inflate the layout for this fragment
 
-
+        if (ContextCompat.checkSelfPermission(
+                requireContext(), android.Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            askForCameraPermission()
+        } else {
+            setupControls()
+        }
         return view
     }
 
@@ -66,6 +90,98 @@ class BusinessCardListFragment : Fragment() {
 
 
     }
+    private fun setupControls() {
+        barcodeDetector =
+            BarcodeDetector.Builder(requireContext()).setBarcodeFormats(Barcode.ALL_FORMATS).build()
+
+        cameraSource = CameraSource.Builder(requireContext(), barcodeDetector)
+            .setRequestedPreviewSize(1920, 1080)
+            .setAutoFocusEnabled(true) //you should add this feature
+            .build()
+
+        binding.cameraSurfaceView.getHolder().addCallback(object : SurfaceHolder.Callback {
+            @SuppressLint("MissingPermission")
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                try {
+                    //Start preview after 1s delay
+                    cameraSource.start(holder)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+
+            @SuppressLint("MissingPermission")
+            override fun surfaceChanged(
+                holder: SurfaceHolder,
+                format: Int,
+                width: Int,
+                height: Int
+            ) {
+                try {
+                    cameraSource.start(holder)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                cameraSource.stop()
+            }
+        })
 
 
+        barcodeDetector.setProcessor(object : Detector.Processor<Barcode> {
+            override fun release() {
+                Toast.makeText(requireContext(), "Scanner has been closed", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            override fun receiveDetections(detections: Detector.Detections<Barcode>) {
+                val barcodes = detections.detectedItems
+                if (barcodes.size() == 1) {
+                    scannedValue = barcodes.valueAt(0).rawValue
+
+
+                    //Don't forget to add this line printing value or finishing activity must run on main thread
+                        cameraSource.stop()
+                        Toast.makeText(requireContext(), "value- $scannedValue", Toast.LENGTH_SHORT).show()
+
+
+                }else
+                {
+                    Toast.makeText(requireContext(), "value- else", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        })
+    }
+
+    private fun askForCameraPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(android.Manifest.permission.CAMERA),
+            requestCodeCameraPermission
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == requestCodeCameraPermission && grantResults.isNotEmpty()) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setupControls()
+            } else {
+                Toast.makeText(requireContext(), "Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraSource.stop()
+    }
 }
+
